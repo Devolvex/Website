eedc63f4a1e61f6f64249f6a7c3ee7da5cfd5318
Zyztem <heyitzyztem@outlook.com>
2023-05-30 16:17:04 -0400

    Very very basic coming soon page!

--- README.md
+++
@@ -1,4 +0,0 @@
-# Devolvex.
-Official website from Devolvex.
-
-This repository has the source code from our website.
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import DevolvexLogo from './assets/devolvex_logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="/" target="_blank">
+          <img src={DevolvexLogo} className="logo" alt="Devolvex logo" />
+        </a>
+      </div>
+      <h1>Coming Soon</h1>
+    </>
+  )
+}
+
+export default App
