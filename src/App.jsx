078f4d4e702d538edc906758f159c255f848edc8
Justinnn <hello@justinnn.dev>
2023-05-31 10:59:31 +0200

    Switched to .webp instead of .png

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <img src="https://asset-cdn.devolvex.net/brands/devolvex_circled.png" className="max-w-sm mx-auto" alt="Devolvex logo" />
+        <img src="https://asset-cdn.devolvex.net/brands/devolvex_circled.webp" className="max-w-sm mx-auto" alt="Devolvex logo" />
         <h1 className="text-4xl mt-6 text-white font-bold font-sans">Coming Soon</h1>
       </div>
     </div>
