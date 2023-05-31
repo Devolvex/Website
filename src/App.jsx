0e2642dea8c7ef3adbf9c83eed1c192d5b92dcb3
Justinnn <hello@justinnn.dev>
2023-05-31 10:50:53 +0200

    feat: switch to external links, instead of local images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import DevolvexLogo from './assets/devolvex_logo.png';
-import './App.css';
-
 function App() {
   return (
-    <div className="dark-bg flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <img src={DevolvexLogo} className="max-w-sm mx-auto" alt="Devolvex logo" />
-        <h1 className="text-4xl mt-6 text-white font-bold">Coming Soon</h1>
+        <img src="https://asset-cdn.devolvex.net/brands/devolvex_circled.png" className="max-w-sm mx-auto" alt="Devolvex logo" />
+        <h1 className="text-4xl mt-6 text-white font-bold font-sans">Coming Soon</h1>
       </div>
     </div>
   );
